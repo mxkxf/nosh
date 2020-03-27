bd7e9f5d9b0e04ee574c5e733840c0b76a9fde51
Mike Francis <mikeffrancis@gmail.com>
2020-03-27 10:56:55 +0000

    Added analytics provider (#1)

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,16 @@ import { Provider } from "react-redux";
 
 import "./styles/index.css";
 import App from "./components/App";
+import AnalyticsProvider from "./components/AnalyticsProvider";
 import store from "./state/store";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <AnalyticsProvider>
+        <App />
+      </AnalyticsProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root"),
