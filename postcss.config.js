b24fa433b1220916248a881830cff4f0b08c361f
Mike Francis <mikeffrancis@gmail.com>
2020-04-17 09:06:34 +0100

    Purge CSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,13 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
+
 module.exports = {
   plugins: [
-    // ...
     require("tailwindcss"),
     require("postcss-nested"),
     require("autoprefixer"),
-    // ...
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
   ],
 };
