6dd672d9ae3b83943564a92559b17fecdc89c10b
Mike Francis <mikeffrancis@gmail.com>
2020-05-05 13:33:24 +0100

    Tailwind upgrade

--- postcss.config.js
+++ postcss.config.js
@@ -3,15 +3,4 @@ module.exports = {
     "tailwindcss",
     "postcss-nested",
     "autoprefixer",
-    process.env.NODE_ENV === "production"
-      ? [
-          "@fullhuman/postcss-purgecss",
-          {
-            content: ["./src/**/*.{js,jsx,ts,tsx}"],
-            defaultExtractor: (content) =>
-              content.match(/[\w-/:]+(?<!:)/g) || [],
-          },
-        ]
-      : undefined,
-  ].filter(Boolean),
 };
