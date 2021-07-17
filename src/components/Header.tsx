7b85a0f821f7811adeb2681e00aaca78928653dd
Mike Francis <mikeffrancis@gmail.com>
2021-07-17 11:41:10 +0100

    Offline notice

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,6 +24,7 @@ const Header: React.FC<
   feeds,
   isCollapsed,
   modal,
+  networkStatus,
   openAboutModal,
   openSubscribeModal,
   selectedFeed,
@@ -118,6 +119,28 @@ const Header: React.FC<
             </HeaderLink>
           </button>
         </nav>
+        {networkStatus === 'OFFLINE' ? (
+          <div className="flex items-center py-3 px-4 bg-red-400 rounded text-white">
+            <span className="block w-4">
+              <svg
+                aria-label="Logo"
+                className="w-4"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M18.364 5.636a9 9 0 010 12.728m0 0l-2.829-2.829m2.829 2.829L21 21M15.536 8.464a5 5 0 010 7.072m0 0l-2.829-2.829m-4.243 2.829a4.978 4.978 0 01-1.414-2.83m-1.414 5.658a9 9 0 01-2.167-9.238m7.824 2.167a1 1 0 111.414 1.414m-1.414-1.414L3 3m8.293 8.293l1.414 1.414"
+                />
+              </svg>
+            </span>
+            <span className="ml-4 hidden md:block">No internet connection</span>
+          </div>
+        ) : null}
         <button onClick={() => openAboutModal()}>
           <HeaderLink>
             <span className="block w-4">
@@ -267,6 +290,7 @@ const mapStateToProps = (state: InitialState) => ({
   modal: state.ui.modal,
   isCollapsed: state.ui.isHeaderCollapsed,
   theme: state.ui.theme,
+  networkStatus: state.ui.networkStatus,
 });
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
