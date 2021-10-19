12d77efd84b84010f0348bc3ab157ca2a48c8fde
Mike Francis <mikeffrancis@gmail.com>
2021-10-19 17:14:35 +0100

    Enable Cypress regression tests (#49)

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 const withPWA = require('next-pwa');
-const runtimeCaching = require('next-pwa/cache');
 
 module.exports = withPWA({
-  pwa: (config, { isServer, dev }) => {
-    if (!isServer && !dev) {
-      return {
-        dest: 'public',
-        runtimeCaching,
-      };
-    }
-
-    return config;
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
   },
 });
