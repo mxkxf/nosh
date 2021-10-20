623d30cfe7fed0e40ca61d1ac1fbae68ce85230f
Mike Francis <mikeffrancis@gmail.com>
2021-10-20 11:29:21 +0100

    More test

--- cypress/integration/app.spec.ts
+++ cypress/integration/app.spec.ts
@@ -26,6 +26,18 @@ describe('App', () => {
     cy.get('button').contains('Dark theme').click();
     cy.wait(1000);
     cy.checkA11y();
+
+    cy.get('button').contains('subscribe').click();
+    cy.wait(1000);
+    cy.checkA11y();
+
+    cy.get('button').contains('Smashing Magazine').click();
+    cy.wait(1000);
+    cy.checkA11y();
+
+    cy.get('article').eq(0).click();
+    cy.wait(1000);
+    cy.checkA11y();
   });
 });
 
