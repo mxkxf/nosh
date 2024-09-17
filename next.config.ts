da952c2d280f88b211639a673555033a7e850408
mxkxf <mikeffrancis@gmail.com>
2024-09-17 10:18:24 +0100

    Update and rename next.config.js to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next'
+ 
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
@@ -8,6 +9,6 @@ const nextConfig = {
       },
     ],
   },
-};
-
-module.exports = nextConfig;
+}
+ 
+export default nextConfig
